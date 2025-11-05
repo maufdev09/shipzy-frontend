2dae5ec39aa76659b0387988a360b122f9f0c4a1
Maruf Ahmed <marufdev09@gmail.com>
2025-11-05 15:27:11 +0600

    refactor: remove commented-out grid layout from DashboardLayout component

--- src/components/LayOut/DashboardLayout.tsx
+++ src/components/LayOut/DashboardLayout.tsx
@@ -24,11 +24,7 @@ export default function DashboardLayout() {
         
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            </div> */}
+          
         
           <Outlet/>
         </div>
